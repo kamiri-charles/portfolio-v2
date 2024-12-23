3c181d1c4ed12f5305131bd69efe4aba45c363c2
Charles Kamiri <charleskamiri6@gmail.com>
2024-12-23 14:44:08 +0300

    fix: Resolve 404 error on refresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route  } from "react-router-dom";
+import { HashRouter as Router, Routes, Route  } from "react-router-dom";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
