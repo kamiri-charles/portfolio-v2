68f950c14fd2db7f0a6d8dfe1f219d6b24a1cded
Charles Kamiri <charleskamiri6@gmail.com>
2025-01-22 14:34:21 +0300

    feat: Add certifications base component

---
+++ src/components/Certifications/index.jsx
@@ -0,0 +1,11 @@
+import "./styles.scss";
+
+const Certifications = () => {
+  return (
+    <div className="certifications">
+        <div className="under-dev">This page is still under development!</div>
+    </div>
+  )
+}
+
+export default Certifications;
